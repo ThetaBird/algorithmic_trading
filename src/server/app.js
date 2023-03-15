82f85b1554718ccc214b737c09c93561b8277903
MaxJikharev <thetaspiration@gmail.com>
2023-03-15 01:28:55 -0400

    Implement API read/write to/from candlestick csv files for future analysis

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
 
 const PORT = 8080;
+const APIURL = "https://api.gemini.com"
 const app = express();
 
-let publicPath = path.resolve(__dirname, "public");
+
+let publicPath = path.resolve(__dirname, "csv");
 app.use(express.static(publicPath));
+
+app.get("/api/v1/:ticker/:timeframe", async (req, res) => {
+    const {ticker, timeframe} = req.params;
+    const filename = `${publicPath}/${ticker}_${timeframe}.csv`;
+    console.log(filename)
+    const fileExists = fs.existsSync(filename);
+    if(!fileExists){
+        const candlesticks = await getCandlesticks(ticker, timeframe);
+        if(candlesticks.length) fs.writeFile(filename, candlesticks.map(candle => candle.join(",")).join("\n"), () => {})
+        res.status(200).send(candlesticks);
+        return;
+    }
+
+    const candlesticks = fs.readFileSync(filename, {encoding:'utf8'});
+
+    res.status(200).send(candlesticks.split("\n").map(candle => candle.split(",")))
+    
+})
+
+const getCandlesticks = async (symbol, timeframe) => {
+    const {status, data} = await axios.get(`${APIURL}/v2/candles/${symbol}/${timeframe}`)
+    if(status == 200) return data;
+    return [];
+}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
